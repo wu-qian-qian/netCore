7c0df810ef225f6c1a040c6135aec6bbab5ce747
wu-qian-qian <103176626+wu-qian-qian@users.noreply.github.com>
2024-03-18 21:18:49 +0800

    Delete Forum.JWT directory

--- Forum.JWT/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Claims;
-
-namespace Forum.JWT
-{
-    public interface ITokenService
-    {
-        string BuildJwtString(IEnumerable<Claim> claims, JWTOptions options);
-    }
-}
